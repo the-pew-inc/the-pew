846f712bac5daf2f102b9990fc7e7c64ec07902b
Stephane Paquet <spaquet@up4b.com>
2022-05-29 23:47:53 -0700

    Mobile menu uses stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,3 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers";
-
-// Start mobile menu
-// const btn = document.querySelector("button.mobile-menu-button");
-// const menu = document.querySelector(".mobile-menu");
-
-// btn.addEventListener("click", () => {
-//   menu.classList.toggle("hidden");
-// });
-// End mobile menu
