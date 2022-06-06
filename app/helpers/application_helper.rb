fbb90e1002f3b6ca24b47c8c23eb6b080dde621d
Stephane Paquet <spaquet@up4b.com>
2022-06-06 15:47:21 -0700

    Flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def flash_bg_color(type)
+    case type
+    when "alert"
+      "bg-red-100"
+    when "notice"
+      "bg-blue-100"
+    else
+      # TODO: report in system log
+      "bg-green-100"
+    end
+  end
+  def flash_text_color(type)
+    case type
+    when "alert"
+      "text-red-700"
+    when "notice"
+      "text-blue-700"
+    else
+      # TODO: report in system log
+      "text-green-700"
+    end
+  end
 end
