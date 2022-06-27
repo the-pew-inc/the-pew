17eaf1e768c8f66e89469d9e9a1955091f7d28c5
Stephane Paquet <spaquet@up4b.com>
2022-06-27 14:40:58 -0700

    Cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -15,16 +15,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_183029) do
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
-    t.bigint "user_id", null: false
     t.string "name"
     t.string "website"
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["country"], name: "index_accounts_on_country"
-    t.index ["user_id"], name: "index_accounts_on_user_id"
   end
-
 
   create_table "active_sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -67,19 +64,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_183029) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "assignments", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "role_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["role_id"], name: "index_assignments_on_role_id"
-    t.index ["user_id"], name: "index_assignments_on_user_id"
-  end
-
   create_table "attendances", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "event_id", null: false
     t.bigint "room_id"
+    t.integer "status", default: 0, null: false
     t.datetime "start_time", null: false
     t.datetime "end_time"
     t.datetime "created_at", null: false
@@ -94,12 +83,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_183029) do
     t.string "name", null: false
     t.datetime "start_date", null: false
     t.datetime "end_date", null: false
+    t.boolean "always_on", default: false, null: false
     t.integer "duration"
     t.integer "event_type", null: false
     t.integer "status", default: 0, null: false
     t.string "short_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["always_on"], name: "index_events_on_always_on"
     t.index ["event_type"], name: "index_events_on_event_type"
     t.index ["short_code"], name: "index_events_on_short_code"
     t.index ["status"], name: "index_events_on_status"
@@ -137,97 +128,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_183029) do
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
-  create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "attendances", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "event_id", null: false
-    t.bigint "room_id"
-    t.datetime "start_time", null: false
-    t.datetime "end_time"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_id"], name: "index_attendances_on_event_id"
-    t.index ["room_id"], name: "index_attendances_on_room_id"
-    t.index ["user_id"], name: "index_attendances_on_user_id"
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name", null: false
-    t.datetime "start_date", null: false
-    t.datetime "end_date", null: false
-    t.integer "duration"
-    t.integer "event_type", null: false
-    t.integer "status", default: 0, null: false
-    t.string "short_code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["event_type"], name: "index_events_on_event_type"
-    t.index ["short_code"], name: "index_events_on_short_code"
-    t.index ["status"], name: "index_events_on_status"
-    t.index ["user_id"], name: "index_events_on_user_id"
-  end
-
-  create_table "profiles", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "nickname"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "room_id", null: false
-    t.string "title", null: false
-    t.integer "status", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["room_id"], name: "index_questions_on_room_id"
-    t.index ["status"], name: "index_questions_on_status"
-    t.index ["user_id", "room_id"], name: "index_questions_on_user_id_and_room_id"
-    t.index ["user_id", "status"], name: "index_questions_on_user_id_and_status"
-    t.index ["user_id"], name: "index_questions_on_user_id"
-  end
-
   create_table "rooms", force: :cascade do |t|
     t.bigint "event_id", null: false
     t.string "name", null: false
+    t.boolean "always_on", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["always_on"], name: "index_rooms_on_always_on"
     t.index ["event_id"], name: "index_rooms_on_event_id"
   end
 
@@ -253,23 +160,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_22_183029) do
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
-  add_foreign_key "accounts", "users"
   add_foreign_key "active_sessions", "users", on_delete: :cascade
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "assignments", "roles"
-  add_foreign_key "assignments", "users"
   add_foreign_key "attendances", "events"
   add_foreign_key "attendances", "users"
   add_foreign_key "events", "users"
   add_foreign_key "members", "accounts"
   add_foreign_key "members", "users"
-  add_foreign_key "active_sessions", "users", on_delete: :cascade
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "attendances", "events"
-  add_foreign_key "attendances", "users"
-  add_foreign_key "events", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "questions", "rooms"
   add_foreign_key "questions", "users"
