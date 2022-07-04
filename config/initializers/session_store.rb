f9eb444d671bba7d2f255f0c2ec677f0284b3d38
Stephane Paquet <spaquet@up4b.com>
2022-07-03 22:39:17 -0700

    Supporting multi-domain cookies

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,4 @@
+Rails.application.config.session_store :cookie_store,
+                                      key: "_thepew_session",
+                                      domain: :all, 
+                                      tld_length: 2
