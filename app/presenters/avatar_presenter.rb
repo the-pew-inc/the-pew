cf2d8d2087aa14e5d8f69354deaad016fafe840f
Stephane Paquet <spaquet@up4b.com>
2022-07-04 11:10:08 -0700

    When a user has no profile picture we default to their gravatar or initials

---
+++ app/presenters/avatar_presenter.rb
@@ -0,0 +1,74 @@
+class AvatarPresenter
+  include ActionView::Context
+  include ActionView::Helpers::AssetTagHelper
+
+  def self.call(user)
+    new(user).call
+  end
+  
+  attr_accessor :user
+  def initialize(user)
+    @user = user
+  end
+
+  def call
+    if gravatar_exists?
+      gravatar_image
+    else
+      initials_element
+    end
+  end
+
+  private
+
+  def gravatar_exists?
+    hash = Digest::MD5.hexdigest(email)
+    http = Net::HTTP.new('www.gravatar.com', 80)
+    http.read_timeout = 2
+    response = http.request_head("/avatar/#{hash}?default=http://gravatar.com/avatar")
+    response.code != '302' ? true : false
+  rescue StandardError, Timeout::Error
+    false
+  end
+
+  def gravatar_image
+    gravatar_id = Digest::MD5.hexdigest(email)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: nickname, class: 'rounded-full')
+  end
+
+  def initials_element
+    style = "background-color: #{avatar_color(initials.first)};"
+    content_tag :div, class: 'rounded-full flex items-center justify-center w-10 h-10', style: style do
+      content_tag :div, initials, class: 'font-mono font-bold text-gray-100'
+    end
+  end
+
+  def email
+    user.email
+  end
+
+  def nickname
+    user.profile.nickname
+  end
+
+  def initials
+    nickname.split.first(2).map(&:first).join
+  end
+
+
+  # Return a color based on the first letter of the initials
+  def avatar_color(initial)
+    colors = [
+      '#00AA55', '#009FD4', '#B381B3', '#939393', '#E3BC00',
+      '#D47500', '#DC2A2A', '#696969', '#ff0000', '#ff80ed',
+      '#407294', '#133337', '#065535', '#c0c0c0', '#5ac18e',
+      '#666666', '#f7347a', '#576675', '#696966', '#008080',
+      '#ffa500', '#40e0d0', '#0000ff', '#003366', '#fa8072',
+      '#800000'
+    ]
+
+    colors[initial.first.to_s.downcase.ord - 97] || '#000000'
+  end
+
+end
