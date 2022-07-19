1b5ffa9ceb18829fd4679fde7640bb6c1d478994
Stephane Paquet <spaquet@up4b.com>
2022-07-19 15:51:21 -0700

    Testing on Heroku

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -5,7 +5,7 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :votable, polymorphic: true
 
-  validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] }
+  # validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] }
 
   enum choice: { up_vote: 1, down_vote: -1, cancel: 0 }
 
