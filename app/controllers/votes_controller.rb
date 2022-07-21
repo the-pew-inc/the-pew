a933aac250d018b0bb1849a557946e17b0bc2257
Stephane Paquet <spaquet@up4b.com>
2022-07-21 16:25:48 -0700

    Some code adjustments

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,24 +4,7 @@ class VotesController < ApplicationController
   def show
     @vote = Vote.find_or_create_by(user_id: current_user.id, votable_id: params[:votable_id], votable_type: params[:votable_type])
   
-    voted(params[:choice])
-
-    votable = @vote.votable
-    target_name = [votable.room_id, votable.class.name.downcase.pluralize]
-    Turbo::StreamsChannel.broadcast_update_later_to(target_name, target: "#{dom_id(votable)}_count", html: votable.vote_count)
-  end
-
-  private
-
-  def voted(choice)
-    return unless choice.in?(Vote::choices.keys)
-
-    case choice
-    when 'up_vote'
-      @vote.up_vote? ? @vote.cancel! : @vote.up_vote!
-    when 'down_vote'
-      @vote.down_vote? ? @vote.cancel! : @vote.down_vote!
-    end
+    @vote.voted(params[:choice])
   end
 
 end
