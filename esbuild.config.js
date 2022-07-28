074561a44bf45041011150d9bb53e2b990615ee2
Stephane Paquet <spaquet@up4b.com>
2022-07-28 15:00:42 -0700

    Removing watch from esbuild as it is passed as an argument in procfile.dev

--- esbuild.config.js
+++ esbuild.config.js
@@ -7,7 +7,7 @@ require("esbuild")
     bundle: true,
     outdir: path.join(process.cwd(), "app/assets/builds"),
     absWorkingDir: path.join(process.cwd(), "app/javascript"),
-    watch: true,
+    // watch: true,
     // custom plugins will be inserted is this array
     plugins: [
       replace({
