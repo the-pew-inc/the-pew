e50fe9a2f837fa1e11617047260335916c9cd204
Stephane Paquet <spaquet@up4b.com>
2022-07-29 15:14:08 -0700

    Updating the schema

--- db/schema.rb
+++ db/schema.rb
@@ -151,11 +151,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_08_163651) do
     t.string "name", null: false
     t.boolean "always_on", default: false, null: false
     t.boolean "allow_anonymous", default: false, null: false
+    t.datetime "start_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["allow_anonymous"], name: "index_rooms_on_allow_anonymous"
     t.index ["always_on"], name: "index_rooms_on_always_on"
     t.index ["event_id"], name: "index_rooms_on_event_id"
+    t.index ["start_date"], name: "index_rooms_on_start_date"
   end
 
   create_table "users", force: :cascade do |t|
