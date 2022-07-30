cb5172210992f223af1b0d4958c732391d5b69e9
Stephane Paquet <spaquet@up4b.com>
2022-07-30 16:09:45 -0700

    Remove the manage question controller and its views

--- test/controllers/manage_questions_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ManageQuestionsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get manage_questions_index_url
-    assert_response :success
-  end
-end
