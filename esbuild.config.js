bbc53389ed862eb4e24ac8371376cc4f3cc8bff6
Stephane Paquet <spaquet@up4b.com>
2022-07-30 12:25:33 -0700

    Better error reporting during the watch loop

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,13 +1,20 @@
 const path = require("path");
 const { replace } = require("esbuild-plugin-replace");
 
+const watch = process.argv.includes("--watch") && {
+  onRebuild(error) {
+    if (error) console.error("[watch] build failed", error);
+    else console.log("[watch] build finished");
+  },
+};
+
 require("esbuild")
   .build({
     entryPoints: ["application.js"],
     bundle: true,
     outdir: path.join(process.cwd(), "app/assets/builds"),
     absWorkingDir: path.join(process.cwd(), "app/javascript"),
-    watch: process.argv.includes("--watch"),
+    watch: watch,
     // custom plugins will be inserted is this array
     plugins: [
       replace({
@@ -16,6 +23,6 @@ require("esbuild")
     ],
   })
   .catch((error) => {
-    console.error(error);
+    console.error("[esbuild] build failed", error);
     process.exit(1);
   });
