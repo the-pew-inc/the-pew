774884fa110944990215434460bbd4e75d540e3a
Stephane Paquet <spaquet@up4b.com>
2022-07-31 02:10:14 -0700

    Fixed a typo

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -55,8 +55,8 @@ class QuestionsController < ApplicationController
     respond_to do |format|
       if @question.update(update_question_params)
 
-        if self.rejected?
-          Message.create(user_id: self.user_id, title: "Question Rejected", content: "Your question: #{self.title} has been rejected with status #{self.rejection_cause}", level: :alert)
+        if @question.rejected?
+          Message.create(user_id: self.user_id, title: "Question Rejected", content: "Your question: #{@question.title} has been rejected with status #{@question.rejection_cause}", level: :alert)
         end
 
         format.turbo_stream
