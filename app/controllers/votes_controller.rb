74ea4303a2a4df0a60bb0c97eb1a44f40c155eae
spaquet <176050+spaquet@users.noreply.github.com>
2022-08-04 07:39:00 -0700

    Fixes (#18)
    * Couple of text changes
    
    * Fixed a UI issue affecting Chrome and Safari
    
    * Fixed some color display
    
    * Basic question export for a given event
    
    * Upgrading view_component
    
    * Updates
    
    * Upgrading to latest esbuild
    
    * Update aws-partition and view components
    
    * Fixed an issue in vote due to the transition from link_to to button_to
    
    * Adding different colors to the voted questions
    
    Co-authored-by: Stephane Paquet <spaquet@up4b.com>

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,6 +5,9 @@ class VotesController < ApplicationController
     @vote = Vote.find_or_create_by(user_id: current_user.id, votable_id: params[:votable_id], votable_type: params[:votable_type])
   
     @vote.voted(params[:choice])
+
+    # For the voter
+    
   end
 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -10,8 +10,12 @@ class Vote < ApplicationRecord
   enum choice: { up_vote: 1, down_vote: -1, cancel: 0 }
 
   def voted(choice)
-    return unless choice.in?(Vote::choices.keys)
-
+    
+    if !choice.in?(Vote::choices.keys)
+      logger.error "User #{self.user_id} tried to use an invalid vote choice: #{choice}"
+      return
+    end
+    
     case choice
     when 'up_vote'
       up_vote? ? cancel! : up_vote!
@@ -23,7 +27,7 @@ class Vote < ApplicationRecord
   # TODO: find a way to make the target independent from the object
   after_update_commit do
     target_name = [votable.room_id, votable.class.name.downcase.pluralize]
-    broadcast_update_later_to(target_name, target: "#{dom_id(votable)}_count", html: votable.vote_count)
+    broadcast_update_later_to(target_name, target: "#{dom_id(votable)}_count", html: votable.up_votes)
   end
 
 end
