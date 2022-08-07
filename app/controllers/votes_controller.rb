d128fa8c16c38f8179387f0865740a75b319d1c9
Stephane Paquet <spaquet@up4b.com>
2022-08-07 16:28:14 -0700

    Vote model is more independent from the votable object

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -8,7 +8,8 @@ class VotesController < ApplicationController
     @vote = Vote.find_or_create_by(user_id: current_user.id, votable_id: params[:votable_id], votable_type: params[:votable_type])
   
     @vote.voted(params[:choice])
-    
+
+    @vote.broadcast_update_later_to([@vote.votable.room.id, :questions], target: "#{dom_id(@vote.votable)}_count", html: @vote.votable.up_votes)
   end
 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -10,26 +10,21 @@ class Vote < ApplicationRecord
   enum choice: { up_vote: 1, down_vote: -1, cancel: 0 }
 
   def voted(choice)
-    
-    if !choice.in?(Vote::choices.keys)
-      logger.error "User #{self.user_id} tried to use an invalid vote choice: #{choice}"
-      return
-    end
-    
     case choice
     when 'up_vote'
       up_vote? ? cancel! : up_vote!
     when 'down_vote'
       down_vote? ? cancel! : down_vote!
+    else
+      logger.error "User #{self.user_id} tried to use an invalid vote choice: #{choice}"
+      return
     end
   end
 
   # TODO: find a way to make the target independent from the object
-  after_update_commit do
-    target_name = [votable.room_id, votable.class.name.downcase.pluralize]
-    broadcast_update_later_to(target_name, target: "#{dom_id(votable)}_count", html: votable.up_votes)
-  end
-
-  
+  # after_update_commit do
+  #   target_name = [votable.room_id, votable.class.name.downcase.pluralize]
+  #   broadcast_update_later_to(target_name, target: "#{dom_id(votable)}_count", html: votable.up_votes)
+  # end
 
 end
