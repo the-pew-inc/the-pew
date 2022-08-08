52231358f127d538ea783e4c15e2c8560c90b46b
Stephane Paquet <spaquet@up4b.com>
2022-08-08 11:11:30 -0700

    Cleaning the Vote.rb model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,11 +1,6 @@
 class Vote < ApplicationRecord
-  include ActionView::RecordIdentifier
-  include ApplicationHelper
-
   belongs_to :user
   belongs_to :votable, polymorphic: true
-
-  # validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] }
 
   enum choice: { up_vote: 1, down_vote: -1, cancel: 0 }
 
@@ -20,11 +15,5 @@ class Vote < ApplicationRecord
       return
     end
   end
-
-  # TODO: find a way to make the target independent from the object
-  # after_update_commit do
-  #   target_name = [votable.room_id, votable.class.name.downcase.pluralize]
-  #   broadcast_update_later_to(target_name, target: "#{dom_id(votable)}_count", html: votable.up_votes)
-  # end
 
 end
