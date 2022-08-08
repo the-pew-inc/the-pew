97574dd56d25f429dac2aa4f9a3234ad766da9ff
Stephane Paquet <spaquet@up4b.com>
2022-08-08 15:58:05 -0700

    Removing the flash template and replacing it by a view component

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,31 +2,28 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import AutoSubmitController from "./auto_submit_controller"
-application.register("auto-submit", AutoSubmitController)
+import AutoSubmitController from "./auto_submit_controller";
+application.register("auto-submit", AutoSubmitController);
 
-import ClipboardController from "./clipboard_controller"
-application.register("clipboard", ClipboardController)
+import ClipboardController from "./clipboard_controller";
+application.register("clipboard", ClipboardController);
 
-import CloseFlashController from "./close_flash_controller"
-application.register("close-flash", CloseFlashController)
+import EmptyStateController from "./empty_state_controller";
+application.register("empty-state", EmptyStateController);
 
-import EmptyStateController from "./empty_state_controller"
-application.register("empty-state", EmptyStateController)
+import ModeController from "./mode_controller";
+application.register("mode", ModeController);
 
-import ModeController from "./mode_controller"
-application.register("mode", ModeController)
+import OrderQuestionsController from "./order_questions_controller";
+application.register("order-questions", OrderQuestionsController);
 
-import OrderQuestionsController from "./order_questions_controller"
-application.register("order-questions", OrderQuestionsController)
+import ToggleController from "./toggle_controller";
+application.register("toggle", ToggleController);
 
-import ToggleController from "./toggle_controller"
-application.register("toggle", ToggleController)
+import UserVoteController from "./user_vote_controller";
+application.register("user-vote", UserVoteController);
 
-import UserVoteController from "./user_vote_controller"
-application.register("user-vote", UserVoteController)
-
-import ValidatePinController from "./validate_pin_controller"
-application.register("validate-pin", ValidatePinController)
+import ValidatePinController from "./validate_pin_controller";
+application.register("validate-pin", ValidatePinController);
