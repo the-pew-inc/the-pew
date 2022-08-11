fa3681a93d695ef42dfca401c1db98ce1fc87353
Stephane Paquet <spaquet@up4b.com>
2022-08-11 16:03:36 -0700

    Charkit.JS

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails";
 import "./controllers";
+import "chartkick/chart.js";
 // import "flowbite";
 
 // document.addEventListener("turbo:load", function () {
