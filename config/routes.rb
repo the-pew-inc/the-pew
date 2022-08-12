22689191594b3872a6cb096e1074682c4f5a8170
Stephane Paquet <spaquet@up4b.com>
2022-08-12 00:41:06 -0700

    Adding username + password protection to Sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   # Adding the sidekiq UI
   require 'sidekiq/web'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_AUTH_USERNAME"])) &
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_AUTH_PASSWORD"]))
+  end if Rails.env.production?
+  
   mount Sidekiq::Web => '/sidekiq'
 
   # Cookie acceptance
