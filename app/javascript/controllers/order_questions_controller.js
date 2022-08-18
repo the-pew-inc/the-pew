c50578a0fb10fa3e3a4b5f8cd3c08dcc01867425
Stephane Paquet <spaquet@up4b.com>
2022-08-18 09:59:59 -0700

    Still experimenting

--- app/javascript/controllers/order_questions_controller.js
+++ app/javascript/controllers/order_questions_controller.js
@@ -2,24 +2,31 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="order-questions"
 export default class extends Controller {
-  static targets = ["item"];
+  static targets = ["question"];
+  static values = ["question"];
 
   connect() {
+    // console.debug("Connecting order-question");
     this.observer = new MutationObserver(this.update.bind(this));
     this.observer.observe(this.element, {
       childList: true,
       attributes: false,
       subtree: true,
+      // characterData: true,
+      // attributeFilter: ["id"],
     });
+    // console.debug("Connecting connect initializing the first update");
     this.update();
   }
 
   disconnect() {
+    console.debug("Disconnecting order-question");
     this.observer.disconnect();
   }
 
-  update() {
-    // console.debug("We are the champions");
-    console.debug(this.itemTargets);
+  update(event) {
+    // console.debug(event);
+    this.questions = this.questionTargets;
+    console.debug(this.questions);
   }
 }
