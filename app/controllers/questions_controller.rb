1a5639ced8573b105a82c5d94e10c0ee112f60f4
Stephane Paquet <spaquet@up4b.com>
2022-09-26 12:51:13 -0700

    Fixing some N+1 queries

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
   # GET /rooms/:room_id/questions
   def index
     if @room.event.universal?
-      @questions = Question.questions_for_room(params[:room_id]).order("id ASC")
+      @questions = Question.questions_for_room(params[:room_id]).order("id ASC").includes(:user, :room)
     else
       # TODO: make it more real ;-)
       redirect_to room_path, alert: "This event is private"
