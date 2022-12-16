8b3596f1d96c723913682809e4ef0d919f164251
spaquet <176050+spaquet@users.noreply.github.com>
2022-12-15 18:54:57 -0800

    New display (#34)
    * Adding vertical margin between questions
    
    * Question new display
    
    * Removed a useless and confusing icon from  the control menu
    
    * gem updates
    
    * Upgrading AWS partitions and kms gems
    
    * Renaming the pool env variable
    
    * Upgrades
    
    * Upgrading esbuild
    
    * Changing how version is displayed in README
    
    * Upgrading postcss and esbuild
    
    * Returning to Puma 5.6.5
    
    * Applying final layout to the room display
    
    * Adding a new status to track the question being answered in a room
    
    * Hiding the room name when __default__
    
    * Layout for image
    
    * Upgrading to support the anonymous
    
    * Adding Favicon
    
    * Adding logo to the Readme
    
    * Fixing a path typo
    
    * Adding list of questions to controller
    
    * Cleaning the README
    
    * Updating the README
    
    * Upgrading aws-partitionsm, irb, loofah, rails-html-anotizer and view_component gems
    
    * Updating the README
    
    * Adding the being answered status to the question list, admin/moderator view
    
    * Upgrading AWS partition gem
    
    * Make sure no more than  one question is marked as beinganswered in a room
    
    * Gem updates
    
    * Upgrading appmap
    
    Signed-off-by: spaquet <176050+spaquet@users.noreply.github.com>
    Co-authored-by: Stephane Paquet <spaquet@up4b.com>

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,16 +2,18 @@ module QuestionsHelper
   def question_status(question)
     case question.status
     when 'asked'
-      content_tag(:span, "in review" , class: "bg-gray-100 text-gray-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300")
+      content_tag(:span, "in review" , class: "bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300")
     when 'approved'
-      content_tag(:span, "asked" , class: "bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800")
+      content_tag(:span, "approved" , class: "bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800")
     when 'answered'
-      content_tag(:span, "answered" , class: "bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900")
+      content_tag(:span, "answered" , class: "bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-900")
     when 'rejected'
       rejection_cause(question)
+    when 'beinganswered'
+      content_tag(:span, "being answered" , class: "bg-indigo-100 text-indigo-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-200 dark:text-indigo-900")
     else
       # TODO: report error
-      content_tag(:span, "invalid status" , class:"bg-yellow-100 text-yellow-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900")
+      content_tag(:span, "invalid status" , class:"bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-200 dark:text-yellow-900")
     end
   end
 
@@ -19,9 +21,9 @@ module QuestionsHelper
 
   def rejection_cause(question)
     if ['duplicate', 'other'].include? question.rejection_cause 
-      content_tag(:span, "rejected - cause: #{question.rejection_cause}" , class: "bg-purple-100 text-purple-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900")
+      content_tag(:span, "rejected - cause: #{question.rejection_cause}" , class: "bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900")
     else
-      content_tag(:span, "rejected - cause: #{question.rejection_cause}" , class: "bg-red-100 text-red-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900")
+      content_tag(:span, "rejected - cause: #{question.rejection_cause}" , class: "bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900")
     end
   end
 end
