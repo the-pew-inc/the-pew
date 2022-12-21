20a1f13745a1b30cd4fa49b3c4d8a91debc1e583
Stephane Paquet <spaquet@up4b.com>
2022-12-21 11:56:30 -0800

    Fixing a seed error

--- db/migrate/20220528004314_create_profiles.rb
+++ db/migrate/20220528004314_create_profiles.rb
@@ -1,6 +1,6 @@
 class CreateProfiles < ActiveRecord::Migration[7.0]
   def change
-    create_table :profiles do |t|
+    create_table :profiles, id: :uuid do |t|
       t.belongs_to :user,     null: false, foreign_key: true, type: :uuid
       t.string     :nickname
       t.integer    :mode,     null: false, default: 0
