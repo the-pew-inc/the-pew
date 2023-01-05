dcec946c6939bd012d45f5736fc53bdea782bebb
Stephane Paquet <spaquet@up4b.com>
2023-01-04 17:23:57 -0800

    Adding better controle about who owns an account

--- db/migrate/20220622183029_create_members.rb
+++ db/migrate/20220622183029_create_members.rb
@@ -1,8 +1,8 @@
 class CreateMembers < ActiveRecord::Migration[7.0]
   def change
     create_table :members, id: :uuid do |t|
-      t.references :user,    null: false, type: :uuid, foreign_key: true
-      t.references :account, null: false, type: :uuid, foreign_key: true
+      t.references :user,    null: false, type: :uuid
+      t.references :account, null: false, type: :uuid
       t.boolean    :owner,   null: false, default: false
 
       t.timestamps
