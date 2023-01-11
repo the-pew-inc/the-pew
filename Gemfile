29ef327d1c870711c6aa6163278cfc3b36d68359
Stephane Paquet <spaquet@up4b.com>
2023-01-10 17:12:30 -0800

    Gem upgrades & better sidekiq initializer

--- Gemfile
+++ Gemfile
@@ -155,7 +155,7 @@ group :development do
   # gem "spring"
 
   # Add support to Rubocop [https://github.com/rubocop/rubocop]
-  gem 'rubocop', '~> 1.30', require: false
+  gem 'rubocop', '~> 1.40', require: false
   gem 'rubocop-rails', require: false
 
   # Add support to Brakeman [https://github.com/presidentbeef/brakeman]
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
+uri = ENV.fetch('REDISCLOUD_URL', 'redis://localhost:6379/1')
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDISCLOUD_URL', 'redis://localhost:6379/1') }
+  config.redis = { url: uri }
 end
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDISCLOUD_URL', 'redis://localhost:6379/1') }
+  config.redis = { url: uri }
 end
