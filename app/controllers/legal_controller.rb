bd699054fee1b663bf2656577fcd604dcb5ea81e
Stephane Paquet <spaquet@up4b.com>
2023-01-12 15:40:29 -0800

    Adding the legal controller and Code of Conduct

---
+++ app/controllers/legal_controller.rb
@@ -0,0 +1,15 @@
+class LegalController < ApplicationController
+
+  # Code of Conduct, Diversity & Inclusion
+  def coc; end
+
+  # Terms of Service
+  def tos; end
+
+  # Terms of Usage
+  def tou; end
+
+  # Privacy
+  def privacy; end
+
+end
