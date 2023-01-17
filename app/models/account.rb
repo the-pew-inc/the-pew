76bab5eb08c5d1325c4ae2cc1099e87a33ca299b
Stephane Paquet <spaquet@up4b.com>
2023-01-17 15:59:46 -0800

    Adding domain and sso fields to Accounts

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,6 +5,10 @@ class Account < ApplicationRecord
   # Tracking changes
   has_paper_trail
 
+  # Callbacks
+  before_save :generate_dns_txt if self.domain_changed? 
+  before_validation :clean_domain if self.domain_changed?
+
   # has_many :members
   has_many :users,   through: :members
   
@@ -16,4 +20,41 @@ class Account < ApplicationRecord
   validates :name,   presence: true, length: { minimum: 3, maximum: 120 }
   validates :logo,   content_type: ['image/png', 'image/jpeg', 'image/jpg', 'image/gif'],
                      size: { between: 1.kilobyte..5.megabytes, message: 'is not given between size' }
+
+  # SSO related field
+  validates :domain,  uniqueness: true, 
+                      fully_qualified_domain: true,
+                      length: { minimum: 3, maximum: 120 }
+  validates :dns_txt, uniqueness: true, length: 63
+
+  # Generate a unique TXT entry
+  def generate_dns_txt
+    self.dns_txt = random_unique_string
+  end
+
+  # Display the full dns text 
+  # This includes the prefix (27 characters) and the unique 63 character string stored in the database
+  def full_dns_txt
+    prefix = "thepew-domain-verification=" # 27 characters
+    prefix + self.dns_txt
+  end
+
+  private
+
+  # Generates a unique 63 character and case sensitive string
+  def random_unique_string
+    rus = ""
+    loop do
+      rus = SecureRandom.hex(63) # or whatever you chose like UUID tools
+      break unless self.class.exists?(dns_txt: rus)
+    end
+    rus
+  end
+
+  # This method removes http and https://, potential trailing / and carriage returns, line feeds, spaces
+  # from the domain name
+  # It is called before validating the model and only if the domain name has changed
+  def clean_domain
+    self.domain = self.domain.gsub(/(http|https):\/\/|\/$/, '').gsub(/[\r\n\s]/, '')
+  end
 end
---
+++ db/migrate/20230117215006_add_sso_fields_to_account.rb
@@ -0,0 +1,14 @@
+class AddSsoFieldsToAccount < ActiveRecord::Migration[7.0]
+  def change
+    add_column :accounts, :domain,  :string
+    add_column :accounts, :sso,     :boolean, null: false, default: false
+    add_column :accounts, :dns_txt, :string
+    add_column :accounts, :domain_verified,    :boolean, null: false, default: false
+    add_column :accounts, :domain_verified_at, :timestamp
+
+    add_index  :accounts, :domain,  unique: true
+    add_index  :accounts, :dns_txt, unique: true
+    add_index  :accounts, :sso
+    add_index  :accounts, :domain_verified
+  end
+end
