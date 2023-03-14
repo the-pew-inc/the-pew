13e1b007874b7e8be79e0a10a8fc1302bbb19453
Stephane Paquet <spaquet@up4b.com>
2023-03-14 13:22:36 -0700

    Reordering the code

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -2,16 +2,16 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="turbo-modal"
 export default class extends Controller {
-  hideModal() {
-    this.element.parentElement.removeAttribute("src");
-    this.element.remove();
-  }
-
   // hide modal on successful form submission
   // action: "turbo:submit-end->turbo-modal#submitEnd"
   submitEnd(e) {
     if (e.detail.success) {
       this.hideModal();
     }
+  }
+
+  hideModal() {
+    this.element.parentElement.removeAttribute("src");
+    this.element.remove();
   }
 }
