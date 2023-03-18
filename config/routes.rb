5c3678b7a99078dd9e953262d7fa541fad885d29
Stephane Paquet <spaquet@up4b.com>
2023-03-18 09:56:37 -0700

    cleaning the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -94,11 +94,6 @@ Rails.application.routes.draw do
     resources :users, only: [:index]
     resource  :ssos,  only: [:show, :update, :edit], shallow: true
   end
-  
-  # get 'organization/:id',      to: 'account#show'
-  # get 'organization/:id/edit', to: 'account#edit'
-  # put 'organization/:id',      to: 'account#update'
-
 
   # Defines the main root path route ("/")
   # Must be the last route in the file
