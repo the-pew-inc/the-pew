6c9a8be09ac51d47648033d546e9e6f8e44d7ae2
Stephane Paquet <spaquet@up4b.com>
2023-03-17 19:23:08 -0700

    Computing the PewPoints

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -6,5 +6,23 @@ class DashboardsController < ApplicationController
     @questions = current_user.questions.order(created_at: :desc).limit(5)
     @status = current_user.questions.group(:status).count
     @total = @status.values.reduce(:+) # Sum all the questions
+    @pew_points = 0
+
+    # Call for @pew_points update
+    update_pew_points
   end
+
+  private
+
+  # TODO: move it to a scheduled task or a counting cache
+  def update_pew_points
+    @questions.each do |question|
+      @votes = question.votes 
+      if @votes.count > 0
+        c = @votes.group(:choice).count
+        @pew_points = @pew_points + c['up_vote']
+      end
+    end
+  end
+
 end
