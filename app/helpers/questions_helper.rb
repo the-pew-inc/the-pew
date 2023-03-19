3a96fe0cd8324b42cd78ecb02feef717dd8966e7
Stephane Paquet <spaquet@up4b.com>
2023-03-18 20:14:08 -0700

    flex-nowrap

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -28,7 +28,7 @@ module QuestionsHelper
     when 'asked'
       content_tag(:span, "Pending" , class: "bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-yellow-300 border border-yellow-300")
     when 'beinganswered'
-      content_tag(:span, "Being Answered" , class: "bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400")
+      content_tag(:span, "Being Answered" , class: "flex-nowrap bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400")
     when 'answered'
       content_tag(:span, "Ansered" , class: "bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-green-400 border border-green-400")
     when 'rejected'
