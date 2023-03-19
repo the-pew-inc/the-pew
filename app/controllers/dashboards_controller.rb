43377123fb5fefea6b49a617280e6221fc381932
Stephane Paquet <spaquet@up4b.com>
2023-03-18 17:49:08 -0700

    Better fix

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,12 +4,16 @@ class DashboardsController < ApplicationController
 
   def show
     @questions = current_user.questions.order(created_at: :desc).limit(5)
-    @status = current_user.questions.group(:status).count.any? ? current_user.questions.group(:status).count : {"asked"=>0, "approved"=>0, "beinganswered"=>0, "answered"=>0, "rejected"=>0}
+    @status = current_user.questions.group(:status).count
     @total = @status.values.reduce(:+) || 0 # Sum all the questions
     @pew_points = 0
 
     # Call for @pew_points update
     update_pew_points
+
+    # Clean @status to make sure it contains the number of expected elements
+    # and that nil is replaced by 0 where needed
+    default_status_count
   end
 
   private
@@ -22,6 +26,18 @@ class DashboardsController < ApplicationController
         c = @votes.group(:choice).count
         @pew_points = @pew_points + c['up_vote']
       end
+    end
+  end
+
+  def default_status_count
+    if @status.empty?
+      @status = {"asked"=>0, "approved"=>0, "beinganswered"=>0, "answered"=>0, "rejected"=>0}
+    else
+      @status['asked']         = 0 if @status['asked'].nil?
+      @status['approved']      = 0 if @status['approved'].nil?
+      @status['beinganswered'] = 0 if @status['beinganswered'].nil?
+      @status['answered']      = 0 if @status['answered'].nil?
+      @status['rejected']      = 0 if @status['rejected'].nil?
     end
   end
 
