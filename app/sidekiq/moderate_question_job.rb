3f35d0295d0dadce71a801691dfa3b1f4319cf3d
Stephane Paquet <spaquet@up4b.com>
2023-03-23 12:43:33 -0700

    Better rejection cause

--- app/sidekiq/moderate_question_job.rb
+++ app/sidekiq/moderate_question_job.rb
@@ -12,19 +12,43 @@ class ModerateQuestionJob
 
     # Send the question to openAI for moderation and wait for the response
     response = client.moderations(parameters: { input: question.dig("title") })  
-    
+
     # Process the response from openAI
     flagged = response.dig("results", 0, "flagged")
+    reasons = response.dig("results", 0, "categories")
     if flagged
       # The question is rejected by the automatic moderation
+      cause = :other
       reasons = response.dig("results", 0, "categories")
       reasons.each do |reason|
-        
+        if reason[0] == "sexual" && reason[1] == true
+          cause = :explicit
+        end
+        if reason[0] == "hate" && reason[1] == true
+          cause = :offensive
+        end
+        if reason[0] == "violence" && reason[1] == true
+          cause = :offensive
+        end
+        if reason[0] == "self-harm" && reason[1] == true
+          cause = :explicit
+        end
+        if reason[0] == "sexual/minors" && reason[1] == true
+          cause = :offensive
+        end
+        if reason[0] == "hate/threatening" && reason[1] == true
+          cause = :offensive
+        end
+        if reason[0] == "violence/graphic" && reason[1] == true
+          cause = :inapropriate
+        end
       end
+
       # Update the question with rejected status
-      Question.update(question.dig("id"), {status: :rejected, rejection_cause: :other, ai_response: response})
+      Question.update(question.dig("id"), {status: :rejected, rejection_cause: cause, ai_response: response})
 
       # Notify the user that their question was rejected
+      
     else
       # The question is approved by the automatic moderation
       Question.update(question.dig("id"), {status: :approved, ai_response: response})
