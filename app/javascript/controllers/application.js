c6d0e09263a8d7b02b80b3aa1e99e0fa5217a136
Stephane Paquet <spaquet@up4b.com>
2023-04-02 11:12:32 -0700

    Honeybadger tracking JS Stimulus too

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,22 @@
 import { Application } from "@hotwired/stimulus";
 import { Autocomplete } from "stimulus-autocomplete";
+import * as Honeybadger from "@honeybadger-io/js";
 
+// Configure honeybadger.js
+Honeybadger.configure({
+  apiKey: HONEYBADGER_API_KEY,
+  environment: "production",
+  revision: "git SHA/project version",
+});
+
+// Start Stimulus App
 const application = Application.start();
+
+// Set up Honeybadger error handler
+application.handleError = (error, message, detail) => {
+  console.warn(message, detail);
+  Honeybadger.notify(error);
+};
 
 // Configure Stimulus development experience
 application.debug = false;
--- esbuild.config.js
+++ esbuild.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-// const { replace } = require("esbuild-plugin-replace");
 
 require("esbuild")
   .context({
@@ -11,6 +10,9 @@ require("esbuild")
     absWorkingDir: path.join(process.cwd(), "app/javascript"),
     plugins: [],
     minify: process.argv.includes("--minify"),
+    define: {
+      HONEYBADGER_API_KEY: JSON.stringify(process.env.HONEYBADGER_API_KEY),
+    },
   })
   .then((context) => {
     if (process.argv.includes("--watch")) {
@@ -24,29 +26,3 @@ require("esbuild")
     }
   })
   .catch(() => process.exit(1));
-
-// const watch = process.argv.includes("--watch") && {
-//   onRebuild(error) {
-//     if (error) console.error("[watch] build failed", error);
-//     else console.log("[watch] build finished");
-//   },
-// };
-
-// require("esbuild")
-//   .build({
-//     entryPoints: ["application.js"],
-//     bundle: true,
-//     outdir: path.join(process.cwd(), "app/assets/builds"),
-//     absWorkingDir: path.join(process.cwd(), "app/javascript"),
-//     watch: watch,
-//     // custom plugins will be inserted is this array
-//     plugins: [
-//       replace({
-//         DOMContentLoaded: "turbo:load",
-//       }),
-//     ],
-//   })
-//   .catch((error) => {
-//     console.error("[esbuild] build failed", error);
-//     process.exit(1);
-//   });
