398ade623853eee995e3051d849f2f195446d587
spaquet <176050+spaquet@users.noreply.github.com>
2023-04-04 11:58:49 -0700

    Recommendations (#63)
    * Rewriting the setting controller
    
    * Better text area
    
    ---------
    
    Co-authored-by: Stephane Paquet <spaquet@up4b.com>

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -6,9 +6,9 @@ class SettingsController < ApplicationController
 
   # GET /settings/
   def index
-    # TODO add a condition for when a user is an admin for the account.
+    # TODO add a condition for when a user is an admin for the account. so that extraction of the organization will be something like current_user.member.organization
     # Current code only displays account information when the user is the owner
-    @organization_id = Member.where(user_id: current_user.id, owner: true).first.organization_id
+    @organization_id = Member.find_by(user_id: current_user.id, owner: true).organization_id
     @organization_owner = true
   end
 end
