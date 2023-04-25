88eb05604495accaf5e38393cd6fd61bb99bde09
Stephane Paquet <spaquet@up4b.com>
2023-04-25 01:35:03 -0700

    Adding Test to Events

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -34,13 +34,46 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  def setup
-    logger.debug "Calling setup"
+  setup do
     @event = events(:one)
-    logger.debug "End setup"
+    @event_invalid_end_date = events(:two)
   end
 
-  test "event belongs to a user" do
-    assert_equal users(:one), @event.user
+  test 'valid event' do
+    assert @event.valid?
+  end
+
+  test 'invalid end date' do
+    refute @event_invalid_end_date.valid?
+    assert_not_nil @event_invalid_end_date.errors[:end_date], 'no validation error for end date present'
+  end
+
+  test 'name length validation' do
+    @event.name = 'a' * 251
+    refute @event.valid?
+    assert_not_nil @event.errors[:name], 'no validation error for name length present'
+  end
+
+  test 'start and end date presence' do
+    @event.start_date = nil
+    @event.end_date = nil
+    refute @event.valid?
+    assert_not_nil @event.errors[:start_date], 'no validation error for start date presence'
+    assert_not_nil @event.errors[:end_date], 'no validation error for end date presence'
+  end
+
+  test 'generates unique short_code' do
+    short_code = @event.short_code
+    new_event = @event.dup
+    new_event.short_code = nil
+    new_event.save
+    refute_equal short_code, new_event.short_code, 'short_code should be unique'
+  end
+
+  test 'sets organization_id based on user_id' do
+    new_event = @event.dup
+    new_event.organization_id = nil
+    new_event.save
+    assert_equal new_event.organization_id, '8c5e36bb-6163-4d07-8a7e-9f13e77d6e40', 'organization_id should be set based on user_id'
   end
 end
