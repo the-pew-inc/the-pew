0fcedccc5d8a1d618d77f54e260a023cea40df2f
Stephane Paquet <spaquet@up4b.com>
2023-04-26 22:05:54 -0700

    Testing the Member model

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -18,7 +18,43 @@
 require "test_helper"
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @john = users(:john)
+    @jane = users(:jane)
+    @organization = organizations(:one)
+  end
+
+  test "should not save member without user" do
+    member = Member.new(organization: @organization)
+    assert_not member.save
+  end
+
+  test "should not save member without organization" do
+    member = Member.new(user: @john)
+    assert_not member.save
+  end
+
+  test "should save member with user and organization" do
+    member = Member.new(user: @john, organization: @organization)
+    assert member.save
+  end
+
+  def test_should_have_one_owner_for_each_organization
+    organization = organizations(:one)
+    assert organization.valid?
+  
+    member1 = members(:john)
+    assert member1.valid?
+  
+    member2 = members(:jane)
+    assert member2.valid?
+  
+    # Check that there is exactly one owner for the organization
+    assert_equal 1, organization.members.where(owner: true).count
+  
+    # Check that both members are part of the organization
+    assert_includes organization.members, member1
+    assert_includes organization.members, member2
+  end
+
 end
