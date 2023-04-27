0d33fbbf1b5f9f286c02cab77b665bb746790a65
Stephane Paquet <spaquet@up4b.com>
2023-04-26 18:40:14 -0700

    Working test for Question

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -38,14 +38,75 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  def setup
-    @question = questions(:first)
+
+  test "title should be present and have length between 3 and 250 characters" do
+    question = Question.new(user: users(:john), room: rooms(:one), organization_id: organizations(:one).id)
+
+    # Test for a valid title
+    question.title = "This is a valid title"
+    assert question.valid?
+
+    # Test for a title that is too short
+    question.title = "a" * 2
+    assert_not question.valid?
+
+    # Test for a title that is too long
+    question.title = "a" * 251
+    assert_not question.valid?
   end
 
-  %i[title].each do |attr|
-    test "#{attr} must be present" do
-      eval "@question.#{attr} = nil"
-      assert_not @question.valid?
+  test "approved_questions_for_room should return approved and answered questions for the given room" do
+    room_one = rooms(:one)
+    approved_questions = room_one.questions.where(status: [:approved, :answered, :beinganswered])
+    assert_equal approved_questions, Question.approved_questions_for_room(room_one)
+  end
+
+  test "asked_questions_for_room should return asked questions for the given room" do
+    room_one = rooms(:one)
+    asked_questions = room_one.questions.where(status: :asked)
+    assert_equal asked_questions, Question.asked_questions_for_room(room_one)
+  end
+
+  test "vote_count should return the total number of votes for a question" do
+    question = questions(:one)
+    assert_equal 2 - 1, question.vote_count
+  end
+  
+  test "up_votes should return the total number of up votes for a question" do
+    question = questions(:one)
+    assert_equal 2, question.up_votes
+  end
+  
+  test "down_votes should return the total number of down votes for a question" do
+    question = questions(:one)
+    assert_equal(-1, question.down_votes)
+  end
+
+  test "name should return the user's nickname if anonymous is false" do
+    question = questions(:one)
+    assert_equal question.user.profile.nickname, question.name
+  end
+
+  test "name should return 'User wished to remain anonymous' if anonymous is true" do
+    question = questions(:two)
+    assert_equal "User wished to remain anonymous", question.name
+  end
+
+  test "email should return the user's email if anonymous is false" do
+    question = questions(:one)
+    assert_equal question.user.email, question.email
+  end
+
+  test "email should return 'User does not want to share their email' if anonymous is true" do
+    question = questions(:two)
+    assert_equal "User does not want to share their email", question.email
+  end
+
+  test "testing question deletion" do
+    assert_difference('Question.count', -1) do
+      question = questions(:one)
+      question.destroy
     end
   end
 end
+
