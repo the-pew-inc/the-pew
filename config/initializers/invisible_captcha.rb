c0b402f1ac405f1782c9b8178391b29d4d0c147e
spaquet <176050+spaquet@users.noreply.github.com>
2023-05-15 10:24:26 -0700

    Fixing a missing invisible_captcha (#79)
    Co-authored-by: Stephane Paquet <spaquet@up4b.com>

--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -1,7 +1,7 @@
 InvisibleCaptcha.setup do |config|
   # config.honeypots           << ['more', 'fake', 'attribute', 'names']
   # config.visual_honeypots    = false
-  config.timestamp_threshold = 2
+  # config.timestamp_threshold = 2
   # config.timestamp_enabled   = true
   # config.injectable_styles   = false
   # config.spinner_enabled     = true
