57aeecd0528f15f8d16ea9ff1b37862380a06187
Stephane Paquet <spaquet@up4b.com>
2023-05-15 19:00:12 -0700

    Adding test to the errors_controller

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should render 404 not found" do
+    get "/404"
+    assert_response 404
+  end
+
+  test "should render 422 unacceptable" do
+    get "/422"
+    assert_response 422
+  end
+
+  test "should render 500 internal server error" do
+    get "/500"
+    assert_response 500
+  end
 end
