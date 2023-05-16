3b1765955a3bc447c2a3eb1a89057b0955db3e93
Stephane Paquet <spaquet@up4b.com>
2023-05-15 19:15:51 -0700

    Merging route changes

--- config/routes.rb
+++ config/routes.rb
@@ -130,6 +130,7 @@ Rails.application.routes.draw do
   resources :subscriptions
   # Webhooks // Stripe
   resources :webhooks, only: :create
+
   # Error pages
   match '/404', to: 'errors#not_found', via: :all
   match '/422', to: 'errors#unacceptable', via: :all
