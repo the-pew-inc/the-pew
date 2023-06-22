c29cb5cc7ae5edf863e34bf59e65151aa65f7c0d
Stephane Paquet <spaquet@up4b.com>
2023-06-21 22:11:46 -0700

    Configuring REDIS to use connection_pool default value 10

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
-uri = ENV.fetch('REDISCLOUD_URL', 'redis://localhost:6379/1')
+URI = ENV.fetch('REDISCLOUD_URL', 'redis://localhost:6379/1')
+REDISCLOUD_POOL_SIZE = ENV.fetch('REDISCLOUD_POOL_SIZE', 10)
+
+# Configuring Redis Connection Pool
+REDIS_POOL = ConnectionPool.new(size: REDISCLOUD_POOL_SIZE) { Redis.new(url: URI) }
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: uri }
+  config.redis = REDIS_POOL
 end
 Sidekiq.configure_client do |config|
-  config.redis = { url: uri }
+  config.redis = REDIS_POOL
 end
