63ec6bee1ec13646715e07edfc8add56a49f6274
spaquet <176050+spaquet@users.noreply.github.com>
2023-06-27 15:56:46 -0700

    fixing the prompt table

--- db/migrate/20230514234427_create_prompts.rb
+++ db/migrate/20230514234427_create_prompts.rb
@@ -1,15 +1,18 @@
 class CreatePrompts < ActiveRecord::Migration[7.0]
   def change
     create_table :prompts, id: :uuid do |t|
+      t.uuid    :organization_id
       t.string  :label,          null: false, limit: 50
       t.string  :title,          null: false, limit: 50
-      t.uuid    :organization_id
-      t.text    :prompt,         null: false
+      t.string  :model
+      t.text    :message,        null: false
+      t.text    :function
 
       t.timestamps
     end
 
     add_index :prompts, [:label, :organization_id], unique: true
     add_index :prompts, :organization_id
+    add_index :prompts, :model
   end
 end
