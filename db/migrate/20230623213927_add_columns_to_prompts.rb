b65675a3a470c649dad373ca2ae08cf16ddb75b2
spaquet <176050+spaquet@users.noreply.github.com>
2023-06-27 15:56:46 -0700

    fixing the prompt table

--- db/migrate/20230623213927_add_columns_to_prompts.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnsToPrompts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :prompts, :model, :string, null: false, default: 'gpt-3.5'
-    add_column :prompts, :function, :string
-
-    # Add an index on the 'model' column
-    add_index :prompts, :model
-  end
-end
