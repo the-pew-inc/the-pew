8e00ef2f7662a331347bff38dc148fa46cd73cc4
spaquet <176050+spaquet@users.noreply.github.com>
2023-07-07 16:25:48 -0700

    Adding a stimulus controller to generate QR event code on the fly

---
+++ app/javascript/controllers/qr_code_generator_controller.js
@@ -0,0 +1,51 @@
+import { Controller } from "@hotwired/stimulus";
+import QRCodeStyling from "qr-code-styling";
+
+// Connects to data-controller="qr-code-generator"
+export default class extends Controller {
+  static targets = ["canvas"];
+  static values = {
+    shortCode: String,
+    size: { type: Number, default: 680 },
+    logoURL: String,
+  };
+
+  connect() {
+    // Get the server URL
+    let serverUrl = window.location.origin;
+
+    // Check if the server URL has a trailing slash
+    if (!serverUrl.endsWith("/")) {
+      // Remove the trailing slash
+      serverUrl = serverUrl.replace(/\/$/, "");
+    }
+
+    // QR Code params
+    let qrCodeParams = {
+      width: this.sizeValue,
+      height: this.sizeValue,
+      type: "svg",
+      data: `${serverUrl}/event/${this.shortCodeValue}?source=from_qr`,
+      dotsOptions: {
+        color: "#4267b2",
+        type: "rounded",
+      },
+      backgroundOptions: {
+        color: "transparent",
+      },
+      imageOptions: {
+        crossOrigin: "anonymous",
+        margin: 20,
+      },
+    };
+
+    // Generate the QRCode and attached it to the target
+    this.qrCode = new QRCodeStyling(qrCodeParams);
+
+    this.qrCode.append(this.canvasTarget);
+  }
+
+  download() {
+    this.qrCode.download({ name: "qr", extension: "svg" });
+  }
+}
