f72b2266aa8b6c8dd4e34c5d7ddcfc2c305beb74
spaquet <176050+spaquet@users.noreply.github.com>
2023-07-13 13:23:42 -0700

    updating the IP Address

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '164.92.82.163', user: 'deploy', roles: %w{app db web}
+server '146.190.133.69', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
