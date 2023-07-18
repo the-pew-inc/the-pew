ecb90c3380bc7ad0785ed8625a62e84b58eac00f
spaquet <176050+spaquet@users.noreply.github.com>
2023-07-17 22:27:08 -0700

    Matching puma socket with nginx

--- config/puma.rb
+++ config/puma.rb
@@ -15,11 +15,12 @@ worker_timeout 3600 if ENV.fetch('RAILS_ENV', 'development') == 'development'
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch('PORT') { 3000 }
+# port ENV.fetch('PORT') { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch('RAILS_ENV') { 'development' }
+rails_env = ENV.fetch('RAILS_ENV') { 'development' }
+environment rails_env
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
@@ -40,4 +41,39 @@ pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 # preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
-plugin :tmp_restart
+# plugin :tmp_restart
+
+if %w[production staging].member?(rails_env)
+  # Logging
+  stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+
+  # Set master PID and state locations
+  pidfile "#{shared_dir}/pids/puma.pid"
+  state_path "#{shared_dir}/pids/puma.state"
+
+  # Change to match your CPU core count
+  workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+  preload_app!
+
+  # Set up socket location
+  bind "unix://#{shared_dir}/sockets/thepew-puma.sock"
+
+  before_fork do
+    # app does not use database, uncomment when needed
+    # ActiveRecord::Base.connection_pool.disconnect!
+  end
+
+  on_worker_boot do
+    ActiveSupport.on_load(:active_record) do
+      # app does not use database, uncomment when needed
+      # db_url = ENV.fetch('DATABASE_URL')
+      # puts "puma: connecting to DB at #{db_url}"
+      # ActiveRecord::Base.establish_connection(db_url)
+    end
+  end
+elsif rails_env == "development"
+  # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+  port   ENV.fetch("PORT") { 3000 }
+  plugin :tmp_restart
+end
