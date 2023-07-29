600e3a214f7550b9d22f109dc4f1f51c96c44d79
spaquet <176050+spaquet@users.noreply.github.com>
2023-07-29 00:47:52 -0700

    better comments

--- app/controllers/poll_participations_controller.rb
+++ app/controllers/poll_participations_controller.rb
@@ -1,8 +1,13 @@
 class PollParticipationsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_poll
+  before_action :set_poll_and_table_data
 
-  # POST /poll_participations
+  # POST /polls/:poll_id/poll_participations
+  # Used to record user's participation to a poll/vote. Once a user
+  # has participated to a poll/vote the user can no longer edit their
+  # answers to that poll/vote. The poll_participations table is used to
+  # track who participated and who did not, making it possible to send
+  # reminders to users who are invited.
   def create
     respond_to do |format|
       if !PollParticipation.participated?(current_user, @poll)
@@ -18,7 +23,7 @@ class PollParticipationsController < ApplicationController
   
   private
 
-  def set_poll
+  def set_poll_and_table_data
     @poll = Poll.find(params[:poll_id])
     @table_data = VoteCounterService.count_by_poll_option_and_choice(@poll)
   end
