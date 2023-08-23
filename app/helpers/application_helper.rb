f9577c8cdbfefb2fd028e9874add40ca38516d57
spaquet <176050+spaquet@users.noreply.github.com>
2023-08-23 16:07:25 -0700

    Adding comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -26,4 +26,57 @@ module ApplicationHelper
   def admin_or_owner?(user)
     user.organization_owner?(user.organization) || user.has_role?(:admin, user.organization)
   end
+
+
+  def resource_status(resource)
+    if resource.draft?
+      # When the resource is still a draft
+      return '<span class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">draft</span>'.html_safe
+    end
+
+    if resource.published?
+      # When the resource is published
+      return '<span class="bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-purple-900 dark:text-purple-300">published</span>'.html_safe
+    end
+
+    if resource.opened?
+      # When the resource is opened (accessible to others than its creator or admins)
+      return '<span class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-green-900 dark:text-green-300">opened</span>'.html_safe
+    end
+
+    if resource.closed?
+      # When the resource is closed
+      return '<span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">closed</span>'.html_safe
+    end
+
+    if resource.archived?
+      # When the resource is archived
+      return '<span class="bg-orange-100 text-orange-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-orange-900 dark:text-orange-300">archived</span>'.html_safe
+    end
+
+    # Default in case we do not cover this type
+    return '<span class="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-gray-900 dark:text-gray-300">?</span>'.html_safe
+  end
+
+  def resource_type(resource)
+    if resource.universal?
+      # User is not confirmed
+      return '<span class="bg-sky-100 text-sky-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-sky-900 dark:text-sky-300">universal</span>'.html_safe
+    end
+
+    if resource.restricted?
+      # User is not confirmed
+      return '<span class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">organization</span>'.html_safe
+    end
+
+    if resource.invite_only?
+      # User is blocked on the platform
+      return '<span class="bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-purple-900 dark:text-purple-300">invite only</span>'.html_safe
+    end
+
+    # If non of the above, the user is active
+    return '<span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-red-900 dark:text-red-300">error</span>'.html_safe
+  end
+
+
 end
