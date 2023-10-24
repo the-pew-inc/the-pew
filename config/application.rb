3564b5d0950ccedd2383120b3a6b8e68d78eef56
spaquet <176050+spaquet@users.noreply.github.com>
2023-10-24 11:14:33 -0700

    7.1.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Ask
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults(7.0)
+    config.load_defaults(7.1)
 
     # Configure ActiveJob to use Sidekiq
     config.active_job.queue_adapter = :sidekiq
