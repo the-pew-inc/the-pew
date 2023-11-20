f84cba5330218aaad68566e708c63b4cc80dfdb0
spaquet <176050+spaquet@users.noreply.github.com>
2023-11-20 11:46:09 -0800

    Fixing #138 (#139)

--- app/sidekiq/summary_extraction_job.rb
+++ app/sidekiq/summary_extraction_job.rb
@@ -1,5 +1,6 @@
 # app/sidekiq/summary_extraction_job.rb
 require 'open-uri'
+require 'uri'
 require 'nokogiri'
 
 class SummaryExtractionJob
@@ -8,21 +9,34 @@ class SummaryExtractionJob
   def perform(url, food_for_thought_id)
     food_for_thought = FoodForThought.find(food_for_thought_id)
     summary = extract_summary_from_url(url)
-    food_for_thought.update(summary: summary)
+    food_for_thought.update(summary:)
   end
 
   private
 
   def extract_summary_from_url(url)
-    doc = Nokogiri::HTML(URI.open(url))
+    # Parse the URL and open it safely
+    doc = URI.parse(url).open { |response| Nokogiri::HTML(response) }
+
+    # Extract meta description
     meta_summary = doc.at('meta[name="description"]')&.[]('content')
     return meta_summary if meta_summary.present?
 
+    # Extract article summary (assuming extract_article_summary is defined elsewhere)
     article_summary = extract_article_summary(doc)
     return article_summary if article_summary.present?
 
-    # If no summary is found, you can provide a default value or return nil
-    return 'No summary available'
+    # Default return value if no summary is found
+    'No summary available'
+  rescue URI::InvalidURIError => e
+    Rails.logger.error("Invalid URL: #{e.message}")
+    'Invalid URL'
+  rescue OpenURI::HTTPError => e
+    Rails.logger.error("HTTP Error: #{e.message}")
+    'Error fetching URL'
+  rescue StandardError => e
+    Rails.logger.error("General Error: #{e.message}")
+    'An error occurred'
   end
 
   def extract_article_summary(doc)
