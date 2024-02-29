abe9c413554b9edf5c1ea6b83337cc9e1b524e35
spaquet <176050+spaquet@users.noreply.github.com>
2024-02-29 15:24:22 -0800

    v2 completed

--- app/controllers/connectors_controller.rb
+++ app/controllers/connectors_controller.rb
@@ -1,3 +1,7 @@
 class ConnectorsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :redirect_if_unauthenticated
+  # before_action :authorize_connector_access, only: %i[index show create]
+
   def index; end
 end
